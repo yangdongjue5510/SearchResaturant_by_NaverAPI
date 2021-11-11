19b4b0d5e937978a26792b2972d0b29ab33bee0b
yangdongjue5510 <djwhy5510@gmail.com>
2021-11-11 15:31:27 +0900

    project bulid

---
+++ src/main/java/com/example/searchresaturant_by_naverapi/SearchResaturantByNaverApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchresaturant_by_naverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchResaturantByNaverApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchResaturantByNaverApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/searchresaturant_by_naverapi/SearchResaturantByNaverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchresaturant_by_naverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchResaturantByNaverApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
